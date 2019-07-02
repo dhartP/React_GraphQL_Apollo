2133c1f6872835804855c0cc475b0d01ef5e8ea1
dhartP <dhartip.developer@gmail.com>
2019-07-02 18:23:49 -0500

    initial commit

---
+++ public/precache-manifest.2ac74dbef5bc82ec273a6d8e0da649bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a688608699c540abf5ea20931b50ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c17d4164c021a6dd9fc",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "bc0a167f39fe5eecd7bc",
+    "url": "/static/js/2.f7f02c83.chunk.js"
+  },
+  {
+    "revision": "1c17d4164c021a6dd9fc",
+    "url": "/static/js/main.d5f4383d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
